9a68b879067c821d5c19f845c5776b546758efbc
Daniel Rocha <to.danielrp@gmail.com>
2018-12-29 23:56:32 -0400

    notifications added

--- src/index.js
+++ src/index.js
@@ -3,21 +3,38 @@ const path = require('path')
 const BrowserWindow = electron.remote.BrowserWindow
 const axios = require('axios')
 const ipc = electron.ipcRenderer
+const notifier = require('node-notifier')﻿
 
 const notifyBtn = document.getElementById('notifyBtn')
 var price = document.querySelector('h1')
 var targetPrice = document.getElementById('targetPrice')
+var targetPriceVal
+
+const notification = {
+  title: 'BTC Alert',
+  body: 'BTC just beat your target price!',
+  icon: path.join(__dirname, '../assets/images/btc.png')
+}
 
 function getBTC() {
   axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC&tsyms=USD')
     .then(res => {
       const cryptos = res.data.BTC.USD
       price.innerHTML = '$'+cryptos.toLocaleString('en')
+      if(targetPrice.innerHTML != '' && targetPriceVal < cryptos) {
+               notifier.notify({
+                   title: 'BTC Alert',
+                   message: 'BTC just beat your target price!',
+                   icon: path.join(__dirname, '../assets/icons/win/icon.ico'),
+                   sound: true,
+                   wait: false
+               })
+      }
     })
 }
 
 getBTC()
-setInterval(getBTC, 30000);
+setInterval(getBTC, 3000);
 
 notifyBtn.addEventListener('click', function(event){
   const modalPath = path.join('file://', __dirname, 'add.html')
