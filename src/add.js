088fac870a5493a1ea7b4ab71d774237e8eeeff2
Daniel Rocha <to.danielrp@gmail.com>
2018-12-29 22:49:01 -0400

    api data retrieve from server

--- src/add.js
+++ src/add.js
@@ -1,10 +1,21 @@
 const electron = require('electron')
 const path = require('path')
 const remote = electron.remote
-
+const ipc = electron.ipcRenderer
 const closeBtn = document.getElementById('closeBtn')
 
 closeBtn.addEventListener('click', function(event) {
+  closeWindow()
+})
+
+const updateBtn = document.getElementById('updateBtn')
+
+updateBtn.addEventListener('click', function() {
+  ipc.send('update-notify-value', document.getElementById('notifyVal').value)
+  closeWindow()
+})
+
+function closeWindow() {
   var window = remote.getCurrentWindow();
   window.close()
-})
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 const electron = require('electron')
 const path = require('path')
 const BrowserWindow = electron.remote.BrowserWindow
+const axios = require('axios')
+const ipc = electron.ipcRenderer
 
 const notifyBtn = document.getElementById('notifyBtn')
+var price = document.querySelector('h1')
+var targetPrice = document.getElementById('targetPrice')
+
+function getBTC() {
+  axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC&tsyms=USD')
+    .then(res => {
+      const cryptos = res.data.BTC.USD
+      price.innerHTML = '$'+cryptos.toLocaleString('en')
+    })
+}
+
+getBTC()
+setInterval(getBTC, 30000);
 
 notifyBtn.addEventListener('click', function(event){
   const modalPath = path.join('file://', __dirname, 'add.html')
@@ -10,4 +25,9 @@ notifyBtn.addEventListener('click', function(event){
   win.on('close', function() { win = null })
   win.loadURL(modalPath)
   win.show()
+})
+
+ipc.on('targetPriceVal', function (event, arg) {
+  targetPriceVal = Number(arg)
+  targetPrice.innerHTML = '$'+targetPriceVal.toLocaleString('en')
 })
